b2aa5dd1c5088451f10fcd3a3249549958b07346
Enrique Martín <emartinm@fdi.ucm.es>
2015-09-25 14:06:08 +0200

    Added original ABS benchmarks and its translation to Haskell from size 5 to 100

---
+++ benchmarks/linear_high/programs/60.hs
@@ -0,0 +1,245 @@
+module Main where
+import ABS
+
+(x:f:xm:ym:n:the_end) = [1..]
+
+main_ :: Method
+main_ [] this wb k = \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) $ \ () -> 
+  Assign x New $ \ () -> 
+  Assign f (Async x m []) k
+
+
+m :: Method
+m [] this wb k = \ () ->
+	Assign xm (Sync ma []) $ \ () ->
+  Assign xm (Sync ma []) $ \ () ->
+  Assign xm (Sync ma []) $ \ () ->
+  Assign xm (Sync ma []) $ \ () ->
+  Assign xm (Sync ma []) $ \ () ->
+  Return xm wb k
+ 	
+ma :: Method
+ma [] this wb k = \ () -> 
+  Assign n New $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Assign n (Attr n) $ \ () -> 
+  Return n wb k
+
+main :: IO ()
+main = run' 1000000 main_ (head the_end)
---
+++ benchmarks/quadratic/programs/50.hs
@@ -0,0 +1,118 @@
+module Main where
+import ABS
+
+(a:f:x:z:the_end) = [1..]
+
+main_ :: Method
+main_ [] this wb k = \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) $ \ () -> Assign a (Sync g []) k
+
+g :: Method
+g [] this wb k = \ () -> Assign x New $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Assign f (Async x m []) $ \ () -> Return x wb k
+
+m :: Method
+m [] this wb k = \ () -> 
+  Assign z New $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Skip $ \ () ->
+  Return z wb k
+
+main :: IO ()
+main = run' 1000000 main_ (head the_end)
